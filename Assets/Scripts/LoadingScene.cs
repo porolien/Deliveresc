89c00ef1f09165d5aed05d87027df0b16e19cbae
Aurelien Tavernier <aurelientav@gmail.com>
2023-05-01 19:04:49 +0200

    Tips End

--- Assets/Scripts/LoadingScene.cs
+++ Assets/Scripts/LoadingScene.cs
@@ -8,13 +8,13 @@ using UnityEngine.SceneManagement;
 public class LoadingScene : MonoBehaviour
 {
     public Image loadingBar;
-    public TextMeshProUGUI TipsText;
-    public List<string> Tips = new List<string>();
+    public Image TipsText;
+    public List<Sprite> Tips = new List<Sprite>();
     private float timeBeforeLast;
     // Start is called before the first frame update
     void Start()
     {
-        TipsText.text = Tips[Random.Range(0, Tips.Count)];
+        TipsText.sprite = Tips[Random.Range(0, Tips.Count)];
     }
 
     // Update is called once per frame
